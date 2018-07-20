f37fdb8abb0c32c20a0992d32900f8d03c304121
mmalaeb <maher.malaeb@murex.com>
2018-07-20 10:22:30 +0300

    Revert "[Sprint-1] Introduce a compilation error to test the CI job"
    This reverts commit 37273d0

--- src/main/java/com/murex/retail/experience/Main.java
+++ src/main/java/com/murex/retail/experience/Main.java
@@ -3,6 +3,6 @@ package com.murex.retail.experience;
 public class Main {
 
    public static void main(String[] args) {
-      Syste.out.println("I still do nothing");
+      System.out.println("I still do nothing");
    }
 }
