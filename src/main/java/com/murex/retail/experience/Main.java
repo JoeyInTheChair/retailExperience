37273d000c033a9ee2e43db89e648b506664f822
mmalaeb <maher.malaeb@murex.com>
2018-07-20 10:04:07 +0300

    [Sprint-1] Introduce a compilation error to test the CI job

--- src/main/java/com/murex/retail/experience/Main.java
+++ src/main/java/com/murex/retail/experience/Main.java
@@ -3,6 +3,6 @@ package com.murex.retail.experience;
 public class Main {
 
    public static void main(String[] args) {
-      System.out.println("I still do nothing");
+      Syste.out.println("I still do nothing");
    }
 }
