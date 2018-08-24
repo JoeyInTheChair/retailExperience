42c57f4062ec5abb0156fe46cfcbaa3d8b53dc15
Georges Kassab <Georges.KASSAB@murex.com>
2018-08-24 13:23:49 +0300

    Training Java Sprint 1 exercise 5

--- src/main/java/com/murex/retail/experience/Main.java
+++ src/main/java/com/murex/retail/experience/Main.java
@@ -1,8 +1,37 @@
 package com.murex.retail.experience;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-   public static void main(String[] args) {
-      System.out.println("I still do nothing");
-   }
+public class Main {
+    public static final String filePath = "src/main/resources/Inventory.csv";
+
+
+    public List<String> mList;
+
+    void requirement1() {
+        Path path = Paths.get(filePath);
+        mList = new ArrayList<String>();
+
+        try {
+            BufferedReader reader = Files.newBufferedReader(path, Charset.forName("UTF-8"));
+
+            String currentLine = null;
+            while ((currentLine = reader.readLine()) != null) {
+                mList.add(currentLine.replace(",", "\t|\t"));
+            }
+
+            for (String line : mList) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/test/java/com/murex/retail/experience/MainTest.java
+++ src/test/java/com/murex/retail/experience/MainTest.java
@@ -1,5 +1,22 @@
 package com.murex.retail.experience;
 
-class MainTest {
+import org.junit.jupiter.api.Test;
 
+class MainTest {
+    @Test
+    void requirement1() {
+        Main main = new Main();
+        if (main.mList == null) {
+            System.out.println("mList\tNULL");
+        } else {
+            System.out.println("mList\tSize = "+main.mList.size());
+        }
+        main.requirement1();
+
+        if (main.mList == null) {
+            System.out.println("mList\tNULL");
+        } else {
+            System.out.println("mList\tSize = "+main.mList.size());
+        }
+    }
 }
