d2619bdba58d5de5e7f8c3a303f0d1c608420fdf
Georges Kassab <Georges.KASSAB@murex.com>
2018-08-27 13:21:08 +0300

    Revert 7f978c1e20b, 153dee5cf23, 42c57f4062e, da2f34a7826 commits

--- src/main/java/com/murex/retail/experience/Main.java
+++ src/main/java/com/murex/retail/experience/Main.java
@@ -1,79 +1,8 @@
 package com.murex.retail.experience;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.Filter;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.appender.ConsoleAppender;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.ConfigurationFactory;
-import org.apache.logging.log4j.core.config.ConfigurationSource;
-import org.apache.logging.log4j.core.config.builder.api.AppenderComponentBuilder;
-import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilder;
-import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.lang.annotation.Target;
-import java.net.URI;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-    public static final String filePath = "src/main/resources/Inventory.csv";
 
-    static final Logger logger = LogManager.getLogger(Main.class.getName());
-
-    public List<String> mList;
-
-    void requirement1() {
-        Path path = Paths.get(filePath);
-        mList = new ArrayList<String>();
-
-        try {
-            BufferedReader reader = Files.newBufferedReader(path, Charset.forName("UTF-8"));
-
-            String currentLine = null;
-            while ((currentLine = reader.readLine()) != null) {
-                mList.add(currentLine.replace(",", "\t|\t"));
-            }
-
-            for (String line : mList) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            System.out.println("requirement1- IOException");
-            e.printStackTrace();
-        }
-    }
-
-    void requirement2() {
-        Path path = Paths.get(filePath);
-        mList = new ArrayList<String>();
-
-        try {
-            BufferedReader reader = Files.newBufferedReader(path, Charset.forName("UTF-8"));
-
-            String currentLine = null;
-            while ((currentLine = reader.readLine()) != null) {
-                mList.add(currentLine.replace(",", "\t|\t"));
-            }
-
-            for (String line : mList) {
-                logger.info(line);
-            }
-        } catch (IOException e) {
-            logger.error("requirement1- IOException");
-            e.printStackTrace();
-        }
-    }
-
-
+   public static void main(String[] args) {
+      System.out.println("I still do nothing");
+   }
 }
-
-
--- src/test/java/com/murex/retail/experience/MainTest.java
+++ src/test/java/com/murex/retail/experience/MainTest.java
@@ -1,17 +1,5 @@
 package com.murex.retail.experience;
 
-import org.junit.jupiter.api.Test;
-
 class MainTest {
-    @Test
-    void requirement1() {
-        Main main = new Main();
-        main.requirement1();
-    }
 
-    @Test
-    void requirement2() {
-        Main main = new Main();
-        main.requirement2();
-    }
 }
