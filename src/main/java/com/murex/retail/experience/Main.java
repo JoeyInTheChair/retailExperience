85e307554247ddc6b50d07bd4f72e6c3ce7b2a53
Joseph Caguioa <jcaguioa@murex.com>
2022-02-08 12:44:13 +0000

    System.out.println replace with logger and outputs dntp.out file

--- src/main/java/com/murex/retail/experience/Main.java
+++ src/main/java/com/murex/retail/experience/Main.java
@@ -1,8 +1,43 @@
 package com.murex.retail.experience;
 
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class Main {
 
-   public static void main(String[] args) {
-      System.out.println("I still do nothing");
+   private static final Logger logger = LogManager.getLogger(Main.class);
+
+   public static void main(String[] args) throws URISyntaxException {
+      LinkedList<String> list = new LinkedList<>();
+      readCSVFileUsingNIO(list);
+      loggerPrint(list);
+   }
+
+   public static void readCSVFileUsingNIO(LinkedList<String> list) throws URISyntaxException {
+      try {
+         List<String> lines = readResourceFile();
+         for(String s : lines)
+            list.add(s.replace(",", "\t|\t"));
+      } catch (IOException ex) {
+         ex.printStackTrace();
+      }
+   }
+
+   private static List<String> readResourceFile() throws IOException, URISyntaxException {
+      return Files.readAllLines(Paths.get(ClassLoader.getSystemResource("Inventory.csv").toURI()));
+   }
+
+   public static void loggerPrint(LinkedList<String> list) {
+
+      for(String s : list) {
+         logger.info(s);
+      }
    }
 }
